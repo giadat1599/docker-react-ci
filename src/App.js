3286146edbbf423e02df2af27b113eae9242018b
giadat1599 <truonggiadat15@gmail.com>
2023-02-05 19:18:00 +0700

    test 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactTTT
+          Learn React
         </a>
       </header>
     </div>
