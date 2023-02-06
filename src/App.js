5bc219186a3e0eb5e68bf719872a9c41f8119092
giadat1599 <truonggiadat15@gmail.com>
2023-02-06 23:22:42 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New feature deployed learn react
         </a>
       </header>
     </div>
